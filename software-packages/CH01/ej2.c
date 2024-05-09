43b064cdb6b15266bd63834fbcc25e94dfeede96
Obijuan <obijuan.cube@gmail.com>
2024-05-09 13:56:36 +0200

    Ch1: add exercise 2

---
+++ software-packages/CH01/ej2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    for (size_t i = 0; i < 3; i++)
+      for (size_t j = 0; j < 0xFFFFFFF; j++) {
+      }
+    
+}
---
+++ software-packages/CH01/ej2.py
@@ -0,0 +1,3 @@
+for i in range(3):
+    for j in range(0xFFFFFFF):
+        pass
