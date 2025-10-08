b647822664e13f1ba26bedc461e71631d23771be
obijuan <obijuan.cube@gmail.com>
2025-10-08 09:39:01 +0200

    LOG: Add example

---
+++ wiki/LOG/Examples/Ej001-unary.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# ──────────────────────────────────────────
+# ── Analisis sintáctico de un lenguaje
+# ── Gramatica:
+# ──  S → o
+# ──────────────────────────────────────────
+
+# ───── Constantes para indicar el lenguaje de entrada
+MSG0 = ""
+MSG1 = "o"
+MSG2 = "oo"
+
+# ────────── Variables globales
+# ── Buffer de entrada
+input_buffer = MSG1
+
+# -- Token actual
+token = ''
+
+# -- Indice para acceder al buffer
+# -- Por defecto apunta al final del buffer
+tokenindex = -1
+
+# ──────────────
+# ── MAIN
+# ──────────────
+
+# -- Apuntar al primer caracter
+tokenindex = tokenindex + 1
+
+# -- Leer el primer caracter
+token = input_buffer[tokenindex]
+
+print(f"Token: {token}")
